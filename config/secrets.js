da7481cd2974d0a12612e076e548494cff75cd6f
Clayton Smith <Cks_jr@yahoo.com>
2016-01-01 17:56:15 -0500

    spelling

--- config/secrets.js
+++ config/secrets.js
@@ -3,13 +3,9 @@
 module.exports = {
     sessionKey: "Your session key",
     emailCredentials: {
-	'no-reply': { // string allows '-' as a key 
-	    user: 'no-reply@pcaso.io',
-            pass: 'Pcasono-reply.io.2015'
-	},
-	'mail': {     // consistency 
-	    user: 'mail@pcaso.io',
-            pass: 'Pcasomail.io.2015'
-	}
+	'account-name': {
+            user: 'accout-name@website.com',
+            pass: 'cool passoword'
+        }
     }
 }
